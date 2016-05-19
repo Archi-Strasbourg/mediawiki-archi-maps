a56b718c64f694077e8921fbe69498ac6286a674
Pierre Rudloff <contact@rudloff.pro>
2016-05-19 18:27:11 +0200

    Initial commit

---
+++ js/map.js
@@ -0,0 +1,24 @@
+var archimap = (function () {
+	return {
+        init: function() {
+            var div = document.getElementById('archimap'),
+                map = L.map(div).setView([48.573392, 7.752353], 13),
+                geo = GeocoderJS.createGeocoder('openstreetmap'),
+                layers = {
+                    'OpenStreetMap': L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                        attribution: '&copy; Contributeurs d\'<a href="http://osm.org/copyright">OpenStreetMap</a>'
+                    }),
+                    'Google Maps (satelitte)': new L.Google('SATELLITE'),
+                    'Google Maps (plan)': new L.Google('ROADMAP')
+                };
+            map.addLayer(layers.OpenStreetMap);
+            map.addControl(new L.Control.Layers(layers, {}));
+            geo.geocode(div.dataset.address + ', ' + div.dataset.city + ', ' + div.dataset.country, function(result) {
+                var coords = [result[0].latitude, result[0].longitude];
+                L.marker(coords).addTo(map);
+                map.setView(coords, 15);
+            });
+        }
+    };
+}());
+window.addEventListener('load', archimap.init, false);
