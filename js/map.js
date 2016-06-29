d5f91da9c2d32a9ae53a747bb9db44942cd4c475
Pierre Rudloff <contact@rudloff.pro>
2016-06-30 00:25:26 +0200

    Lint

--- js/map.js
+++ js/map.js
@@ -1,19 +1,22 @@
+/*jslint browser: true*/
+/*global L, GeocoderJS, window*/
 var archimap = (function () {
-	return {
-        init: function() {
-            var div = document.getElementById('archimap'),
-                map = L.map(div, { fullscreenControl: true }).setView([48.573392, 7.752353], 13),
-                geo = GeocoderJS.createGeocoder('openstreetmap'),
-                layers = {
-                    'OpenStreetMap': L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                        attribution: '&copy; Contributeurs d\'<a href="http://osm.org/copyright">OpenStreetMap</a>'
-                    }),
-                    'Google Maps (satelitte)': new L.Google('SATELLITE'),
-                    'Google Maps (plan)': new L.Google('ROADMAP')
-                };
+    "use strict";
+    return {
+        init: function () {
+            var div = document.getElementById("archimap");
+            var map = L.map(div, {fullscreenControl: true}).setView([48.573392, 7.752353], 13);
+            var geo = GeocoderJS.createGeocoder("openstreetmap");
+            var layers = {
+                "OpenStreetMap": L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+                    attribution: "&copy; Contributeurs d'<a href=\"http://osm.org/copyright\">OpenStreetMap</a>"
+                }),
+                "Google Maps (satelitte)": new L.Google("SATELLITE"),
+                "Google Maps (plan)": new L.Google("ROADMAP")
+            };
             map.addLayer(layers.OpenStreetMap);
             map.addControl(new L.Control.Layers(layers, {}));
-            geo.geocode(div.dataset.address + ', ' + div.dataset.city + ', ' + div.dataset.country, function(result) {
+            geo.geocode(div.dataset.address + ", " + div.dataset.city + ", " + div.dataset.country, function (result) {
                 var coords = [result[0].latitude, result[0].longitude];
                 L.marker(coords).addTo(map);
                 map.setView(coords, 15);
@@ -21,4 +24,4 @@ var archimap = (function () {
         }
     };
 }());
-window.addEventListener('load', archimap.init, false);
+window.addEventListener("load", archimap.init, false);
