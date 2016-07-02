bddf7be183660975a7227e59c0e5438e1b2d605b
Pierre Rudloff <contact@rudloff.pro>
2016-07-02 18:57:35 +0200

    Display all addresses on map

--- js/map.js
+++ js/map.js
@@ -1,18 +1,61 @@
-/*jslint browser: true*/
+/*jslint browser: true, for: true*/
 /*global L, GeocoderJS, window, mw*/
 var archimap = (function () {
     "use strict";
+    var addresses;
+    var map;
+    var api;
+
     function openAddress(e) {
         window.location = mw.config.get("wgArticlePath").replace("$1", e.target.title);
     }
+
     function setTitle(e) {
         e.target._container.setAttribute("title", e.target.title.replace("Adresse:", ""));
     }
+
+    function addMarker(result) {
+        var coords = [result[0].latitude, result[0].longitude];
+        L.marker(coords, {clickable: false}).addTo(addresses);
+    }
+
+    function addMarkerAndOther(result) {
+        var coords = [result[0].latitude, result[0].longitude];
+        L.marker(coords, {clickable: false}).addTo(addresses);
+        api.get({
+            action: "query",
+            list: "geosearch",
+            gsradius: 10000,
+            gscoord: result[0].latitude + "|" + result[0].longitude,
+            gsnamespace: 4000
+        }).done(function (data) {
+            data.query.geosearch.forEach(
+                function (item, i) {
+                    if (item.title.replace("Adresse:", "") !== mw.config.get("wgTitle")) {
+                        var marker = L.circle(
+                            item,
+                            5,
+                            {
+                                title: item.title.replace("Adresse:", "")
+                            }
+                        ).addTo(addresses);
+                        marker.on("add", setTitle);
+                        marker.title = item.title;
+                        marker.on("click", openAddress);
+                        if (i === data.query.geosearch.length - 1) {
+                            map.fitBounds(addresses.getBounds());
+                        }
+                    }
+                }
+            );
+        });
+    }
+
     return {
         init: function () {
             var div = document.getElementById("archimap");
             if (div) {
-                var map = L.map(div, {fullscreenControl: true});
+                map = L.map(div, {fullscreenControl: true});
                 var geo = GeocoderJS.createGeocoder("openstreetmap");
                 var layers = {
                     "OpenStreetMap": L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -21,41 +64,20 @@ var archimap = (function () {
                     "Google Maps (satelitte)": new L.Google("SATELLITE"),
                     "Google Maps (plan)": new L.Google("ROADMAP")
                 };
-                var api = new mw.Api();
-                var addresses = L.featureGroup().addTo(map);
+                addresses = L.featureGroup().addTo(map);
+                api = new mw.Api();
+                var i;
                 map.addLayer(layers.OpenStreetMap);
                 map.addControl(new L.Control.Layers(layers, {}));
-                geo.geocode(div.dataset.address + ", " + div.dataset.city + ", " + div.dataset.country, function (result) {
-                    var coords = [result[0].latitude, result[0].longitude];
-                    L.marker(coords, {clickable: false}).addTo(addresses);
-                    api.get({
-                        action: "query",
-                        list: "geosearch",
-                        gsradius: 10000,
-                        gscoord: result[0].latitude + "|" + result[0].longitude,
-                        gsnamespace: 4000
-                    }).done(function (data) {
-                        data.query.geosearch.forEach(
-                            function (item, i) {
-                                if (item.title.replace("Adresse:", "") !== mw.config.get("wgTitle")) {
-                                    var marker = L.circle(
-                                        item,
-                                        5,
-                                        {
-                                            title: item.title.replace("Adresse:", "")
-                                        }
-                                    ).addTo(addresses);
-                                    marker.on("add", setTitle);
-                                    marker.title = item.title;
-                                    marker.on("click", openAddress);
-                                    if (i === data.query.geosearch.length - 1) {
-                                        map.fitBounds(addresses.getBounds());
-                                    }
-                                }
-                            }
-                        );
-                    });
-                });
+                for (i = 1; i < 10; i += 1) {
+                    if (div.dataset["address" + i]) {
+                        if (i === 1) {
+                            geo.geocode(div.dataset["address" + i] + ", " + div.dataset.city + ", " + div.dataset.country, addMarkerAndOther);
+                        } else {
+                            geo.geocode(div.dataset["address" + i] + ", " + div.dataset.city + ", " + div.dataset.country, addMarker);
+                        }
+                    }
+                }
             }
         }
     };
