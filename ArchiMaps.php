faa4f30c10658f80fe50ce548c1c88ce31b3a341
Pierre Rudloff <contact@rudloff.pro>
2016-07-23 23:14:26 +0200

    Use Google Maps API key

--- ArchiMaps.php
+++ ArchiMaps.php
@@ -6,7 +6,7 @@ class ArchiMaps
 {
     public static function addScripts(\OutputPage &$out, \Skin &$skin)
     {
-        global $wgScriptPath;
+        global $wgScriptPath, $egMapsGMaps3ApiKey;
         if (in_array('ext.maps.leaflet', $out->getModules())) {
             $out->addStyle(
                 $wgScriptPath.'/extensions/ArchiMaps/bower_components/leaflet-fullscreen/dist/leaflet.fullscreen.css'
@@ -14,7 +14,7 @@ class ArchiMaps
             $out->addScriptFile(
                 $wgScriptPath.'/extensions/ArchiMaps/bower_components/leaflet-fullscreen/dist/Leaflet.fullscreen.min.js'
             );
-            $out->addScriptFile('https://maps.google.com/maps/api/js?v=3');
+            $out->addScriptFile('https://maps.google.com/maps/api/js?v=3&key='.$egMapsGMaps3ApiKey);
             $out->addScriptFile(
                 $wgScriptPath.'/extensions/ArchiMaps/bower_components/leaflet-plugins/layer/tile/Google.js'
             );
