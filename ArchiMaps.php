02fe9460e1ea817639cd02c49132d0e613f29d3e
Pierre Rudloff <contact@rudloff.pro>
2017-04-06 12:20:15 +0200

    Use separate Google Maps API key

--- ArchiMaps.php
+++ ArchiMaps.php
@@ -17,9 +17,9 @@ class ArchiMaps
      */
     public static function addScripts(\OutputPage &$out)
     {
-        global $wgScriptPath, $egMapsGMaps3ApiKey;
+        global $wgScriptPath, $egArchiMapsGMapsKey;
         if (in_array('ext.maps.leaflet', $out->getModules())) {
-            $out->addScriptFile('https://maps.google.com/maps/api/js?v=3&key='.$egMapsGMaps3ApiKey);
+            $out->addScriptFile('https://maps.google.com/maps/api/js?v=3&key='.$egArchiMapsGMapsKey);
             $out->addScriptFile(
                 $wgScriptPath.'/extensions/ArchiMaps/bower_components/Leaflet.GridLayer.GoogleMutant/Leaflet.GoogleMutant.js'
             );
