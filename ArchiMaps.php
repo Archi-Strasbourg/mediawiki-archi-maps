b93f057bb8a58c2364905244463f0e81313c54de
Pierre Rudloff <contact@rudloff.pro>
2017-04-29 19:06:40 +0200

    Lint

--- ArchiMaps.php
+++ ArchiMaps.php
@@ -21,13 +21,16 @@ class ArchiMaps
         if (in_array('ext.maps.leaflet', $out->getModules())) {
             $out->addScriptFile('https://maps.google.com/maps/api/js?v=3&key='.$egArchiMapsGMapsKey);
             $out->addScriptFile(
-                $wgScriptPath.'/extensions/ArchiMaps/bower_components/Leaflet.GridLayer.GoogleMutant/Leaflet.GoogleMutant.js'
+                $wgScriptPath.
+                '/extensions/ArchiMaps/bower_components/Leaflet.GridLayer.GoogleMutant/Leaflet.GoogleMutant.js'
             );
             $out->addScriptFile(
-                $wgScriptPath.'/extensions/ArchiMaps/bower_components/leaflet-control-geocoder/dist/Control.Geocoder.js'
+                $wgScriptPath.
+                '/extensions/ArchiMaps/bower_components/leaflet-control-geocoder/dist/Control.Geocoder.js'
             );
             $out->addStyle(
-                $wgScriptPath.'/extensions/ArchiMaps/bower_components/leaflet-control-geocoder/dist/Control.Geocoder.css'
+                $wgScriptPath.
+                '/extensions/ArchiMaps/bower_components/leaflet-control-geocoder/dist/Control.Geocoder.css'
             );
             $out->addScriptFile($wgScriptPath.'/extensions/ArchiMaps/js/map.js');
         }
--- js/map.js
+++ js/map.js
@@ -1,45 +1,49 @@
 /*jslint browser: true, for: true*/
-/*global L, window, mw*/
+/*global L, window, mw, maps, $*/
 var archimap = (function () {
-    'use strict';
-    return {
-        init: function () {
-            var map = maps.leafletList[0].map;
-            var OSMlayer;
-            maps.leafletList[0].map.eachLayer(
-                function (layer) {
-                    if (layer._url) {
-                        OSMlayer = layer;
-                    }
+    "use strict";
+
+    function init() {
+        var map = maps.leafletList[0].map;
+        var OSMlayer;
+        map.eachLayer(
+            function (layer) {
+                if (layer._url) {
+                    OSMlayer = layer;
                 }
-            );
-            var layers = {
-                "OpenStreetMap": OSMlayer,
-                "Google Maps (satelitte)": L.gridLayer.googleMutant({ type: "satellite"}),
-                "Google Maps (plan)": L.gridLayer.googleMutant({ type: "roadmap"})
-            };
-            map.addLayer(layers.OpenStreetMap);
-            map.addControl(new L.Control.Layers(layers, {}));
-            map.addControl(new L.Control.geocoder());
-            var defaultLayer = mw.user.options.get('map-layer');
-            if (defaultLayer) {
-                layers[defaultLayer].addTo(map);
             }
-        },
-        pollForMap (time) {
-            if ($('#map_leaflet_1').length > 0) {
-                if (maps && maps.leafletList[0] && maps.leafletList[0].map) {
-                    archimap.init();
-                } else {
-                    if (!time) {
-                        time = 50;
-                    }
-                    setTimeout(function () {
-                        archimap.pollForMap(time * 2)
-                    }, time);
+        );
+        var layers = {
+            "OpenStreetMap": OSMlayer,
+            "Google Maps (satelitte)": L.gridLayer.googleMutant({type: "satellite"}),
+            "Google Maps (plan)": L.gridLayer.googleMutant({type: "roadmap"})
+        };
+        map.addLayer(layers.OpenStreetMap);
+        map.addControl(new L.Control.Layers(layers, {}));
+        map.addControl(L.Control.geocoder());
+        var defaultLayer = mw.user.options.get("map-layer");
+        if (defaultLayer) {
+            layers[defaultLayer].addTo(map);
+        }
+    }
+
+    function pollForMap(time) {
+        if ($("#map_leaflet_1").length > 0) {
+            if (maps && maps.leafletList[0] && maps.leafletList[0].map) {
+                init();
+            } else {
+                if (!time) {
+                    time = 50;
                 }
+                setTimeout(function () {
+                    pollForMap(time * 2);
+                }, time);
             }
         }
+    }
+
+    return {
+        pollForMap: pollForMap
     };
 }());
-window.addEventListener('load', archimap.pollForMap, false);
+window.addEventListener("load", archimap.pollForMap, false);
