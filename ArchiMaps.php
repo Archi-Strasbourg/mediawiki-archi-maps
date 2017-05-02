78c4c97e2fe0c0c3f92bb154122402ef546299bc
Pierre Rudloff <contact@rudloff.pro>
2017-05-02 19:18:46 +0200

    Cleaner way to load resources

--- ArchiMaps.php
+++ ArchiMaps.php
@@ -17,23 +17,9 @@ class ArchiMaps
      */
     public static function addScripts(\OutputPage &$out)
     {
-        global $wgScriptPath, $egArchiMapsGMapsKey;
-        if (in_array('ext.maps.leaflet', $out->getModules())) {
-            $out->addScriptFile('https://maps.google.com/maps/api/js?v=3&key='.$egArchiMapsGMapsKey);
-            $out->addScriptFile(
-                $wgScriptPath.
-                '/extensions/ArchiMaps/bower_components/Leaflet.GridLayer.GoogleMutant/Leaflet.GoogleMutant.js'
-            );
-            $out->addScriptFile(
-                $wgScriptPath.
-                '/extensions/ArchiMaps/bower_components/leaflet-control-geocoder/dist/Control.Geocoder.js'
-            );
-            $out->addStyle(
-                $wgScriptPath.
-                '/extensions/ArchiMaps/bower_components/leaflet-control-geocoder/dist/Control.Geocoder.css'
-            );
-            $out->addScriptFile($wgScriptPath.'/extensions/ArchiMaps/js/map.js');
-        }
+        global $egArchiMapsGMapsKey;
+        $out->addScriptFile('https://maps.google.com/maps/api/js?v=3&key='.$egArchiMapsGMapsKey);
+        $out->addModules('ext.archimaps');
     }
 
     public static function addPreferences(\User $user, array &$preferences)
