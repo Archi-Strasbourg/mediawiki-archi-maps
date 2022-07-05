ae7cfc2446f196aa5100884aa8701182fa6cbe01
Pierre Rudloff <prudloff@insite.coop>
2022-07-05 11:58:47 +0200

    Lien généré en JS (Insite #30324) + refactorisation

--- js/map.js
+++ js/map.js
@@ -1,71 +1,94 @@
 /*jslint browser: true, for: true*/
 /*global L, window, mw, maps, $*/
-var archimap = (function () {
+
+const archimap = (function () {
     "use strict";
 
     const api = new mw.Api();
+
+    /**
+     * @param {object} popup
+     * @param {string} html
+     */
+    function addImage(popup, html) {
+        popup.setContent(popup.getContent() + html);
+        popup.hasImage = true;
+    }
+
+    /**
+     * @param {string[]} dates
+     * @param {object} date
+     */
+    function addDate(dates, date) {
+        dates.push(date.fulltext);
+    }
+
+    /**
+     * @param {string[]} names
+     * @param {object} person
+     */
+    function addPerson(names, person) {
+        // noinspection JSNonASCIINames
+        if (person['Métier'].item[0] === 'Architecte') {
+            names.push(person['Nom'].item[0].fulltext.replace('Personne:', ''));
+        }
+    }
+
+    /**
+     * @param {object} popup
+     * @param {object} data
+     */
+    function handleAddressInfo(popup, data) {
+        const results = Object.values(data.query.results);
+        const filename = results[0].printouts['Image principale'][0].fulltext;
+
+        let text = '';
+
+        const dates = [];
+        results[0].printouts['Date de construction'].forEach(addDate.bind(null, dates));
+        if (dates.length > 0) {
+            text += 'Date de construction&nbsp;: ' + dates.join(', ') + '<br/>';
+        }
+
+        const names = [];
+        results[0].printouts['Personne'].forEach(addPerson.bind(null, names));
+        if (names.length > 0) {
+            text += 'Architecte&nbsp;: ' + names.join(', ') + '<br/>';
+        }
+
+        text += '[[' + filename + '|center|200px]]';
+
+        api.parse(text).done(addImage.bind(null, popup));
+    }
+
+    /**
+     * @param {object} popup
+     * @param {string} title
+     * @param {string} html
+     * @return {jQuery.Promise|*}
+     */
+    function insertLink(popup, title, html) {
+        popup.setContent(html);
+
+        api.get({
+            action: 'ask',
+            query: '[[' + 'Adresse:' + title + ']]|?Image principale|?Date de construction|?Personne'
+        }).done(handleAddressInfo.bind(null, popup));
+    }
 
     /**
      * @param {object} e
      */
     function customizePopup(e) {
         if (!e.popup.hasImage) {
-            const title = jQuery(e.popup.getContent()).find('a').attr('title');
+            /*
+            On ne met pas directement le lien dans le JSON généré par Maps
+            parce que ça alourdit pas mal le JSON.
+             */
+            const title = jQuery(e.popup.getContent()).text();
+
             if (typeof title != 'undefined') {
-                api.get({
-                    action: 'ask',
-                    query: '[[' + title + ']]|?Image principale|?Date de construction|?Personne'
-                    // Obligé de faire des fonctions anonymes pour garder la référence au popup.
-                }).done(
-                    /**
-                     * @param {object} data
-                     */
-                    function (data) {
-                        const results = Object.values(data.query.results);
-                        const filename = results[0].printouts['Image principale'][0].fulltext;
-
-                        let text = '';
-
-                        const dates = [];
-                        results[0].printouts['Date de construction'].forEach(
-                            function (date) {
-                                dates.push(date.fulltext);
-                            }
-                        );
-                        if (dates.length > 0) {
-                            text += 'Date de construction&nbsp;: ' + dates.join(', ') + '<br/>';
-                        }
-
-                        const names = [];
-                        results[0].printouts['Personne'].forEach(
-                            function (person) {
-                                // noinspection JSNonASCIINames
-                                if (person['Métier'].item[0] === 'Architecte') {
-                                    names.push(person['Nom'].item[0].fulltext.replace('Personne:', ''));
-                                }
-                            }
-                        );
-                        if (names.length > 0) {
-                            text += 'Architecte&nbsp;: ' + names.join(', ') + '<br/>';
-                        }
-
-                        text += '[[' + filename + '|center|200px]]';
-
-                        api.get({
-                            action: 'parse',
-                            contentmodel: 'wikitext',
-                            text: text
-                        }).done(
-                            /**
-                             * @param {object} result
-                             */
-                            function addImage(result) {
-                                e.popup.setContent(e.popup.getContent() + result.parse.text['*']);
-                                e.popup.hasImage = true;
-                            }
-                        );
-                    }
-                );
+                api.parse("'''[[" + 'Adresse:' + title + "]]'''").done(insertLink.bind(null, e.popup, title));
             }
         }
     }
